d6e2693bdc5483f087825c7f1c518755f2cd0440
fesh0r <dev@fesh.geek.nz>
2012-01-29 17:06:33 +1300

    reinstate deleted methods in MpaSignature that i removed for some stupid reason when updating to ASM 4.0 RC2 so instead of outputing
    !c Ljava/lang/Enum<Lnet/minecraft/src/EnumAction;>; => Ljava/lang/EnumLjq
    !m (Ljava/lang/String;I)V => Ljava/lang/String
    
    we get
    
    !c Ljava/lang/Enum<Lnet/minecraft/src/EnumAction;>; => Ljava/lang/Enum<Ljq;>;
    !m (Ljava/lang/String;I)V => (Ljava/lang/String;I)V
    
    also update copyright dates to match ASM 4.0 final and bump main version

--- src/main/java/COM/rl/MapSignatureAdapter.java
+++ src/main/java/COM/rl/MapSignatureAdapter.java
@@ -24,6 +24,116 @@ public class MapSignatureAdapter extends SignatureVisitor
         this.nm = nm;
     }
 
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitFormalTypeParameter(java.lang.String)
+     */
+    @Override
+    public void visitFormalTypeParameter(String name) throws SignatureException
+    {
+        this.sv.visitFormalTypeParameter(name);
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitClassBound()
+     */
+    @Override
+    public SignatureVisitor visitClassBound() throws SignatureException
+    {
+        this.sv.visitClassBound();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitInterfaceBound()
+     */
+    @Override
+    public SignatureVisitor visitInterfaceBound() throws SignatureException
+    {
+        this.sv.visitInterfaceBound();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitSuperclass()
+     */
+    @Override
+    public SignatureVisitor visitSuperclass() throws SignatureException
+    {
+        this.sv.visitSuperclass();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitInterface()
+     */
+    @Override
+    public SignatureVisitor visitInterface() throws SignatureException
+    {
+        this.sv.visitInterface();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitParameterType()
+     */
+    @Override
+    public SignatureVisitor visitParameterType() throws SignatureException
+    {
+        this.sv.visitParameterType();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitReturnType()
+     */
+    @Override
+    public SignatureVisitor visitReturnType() throws SignatureException
+    {
+        this.sv.visitReturnType();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitExceptionType()
+     */
+    @Override
+    public SignatureVisitor visitExceptionType() throws SignatureException
+    {
+        this.sv.visitExceptionType();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitBaseType(char)
+     */
+    @Override
+    public void visitBaseType(char descriptor) throws SignatureException
+    {
+        this.sv.visitBaseType(descriptor);
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitTypeVariable(java.lang.String)
+     */
+    @Override
+    public void visitTypeVariable(String name) throws SignatureException
+    {
+        this.sv.visitTypeVariable(name);
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitArrayType()
+     */
+    @Override
+    public SignatureVisitor visitArrayType() throws SignatureException
+    {
+        this.sv.visitArrayType();
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitClassType(java.lang.String)
+     */
     @Override
     public void visitClassType(String name) throws SignatureException
     {
@@ -40,6 +150,9 @@ public class MapSignatureAdapter extends SignatureVisitor
         this.sv.visitClassType(newName);
     }
 
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitInnerClassType(java.lang.String)
+     */
     @Override
     public void visitInnerClassType(String name) throws SignatureException
     {
@@ -54,5 +167,33 @@ public class MapSignatureAdapter extends SignatureVisitor
             throw new SignatureException(e);
         }
         this.sv.visitInnerClassType(newName);
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitTypeArgument()
+     */
+    @Override
+    public void visitTypeArgument() throws SignatureException
+    {
+        this.sv.visitTypeArgument();
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitTypeArgument(char)
+     */
+    @Override
+    public SignatureVisitor visitTypeArgument(char wildcard) throws SignatureException
+    {
+        this.sv.visitTypeArgument(wildcard);
+        return this;
+    }
+
+    /**
+     * @see org.objectweb.asm.signature.SignatureVisitor#visitEnd()
+     */
+    @Override
+    public void visitEnd() throws SignatureException
+    {
+        this.sv.visitEnd();
     }
 }
